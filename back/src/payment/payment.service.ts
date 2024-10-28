5e6648f1bd4c0f51febb92d326ce623766fc250c
MatiasAhumada <mahumada486@gmail.com>
2024-10-28 17:10:59 -0300

    cambie basticks por comillas en la redireccion del back para ver si soluciono el problema dela url

--- back/src/payment/payment.service.ts
+++ back/src/payment/payment.service.ts
@@ -25,9 +25,9 @@ export class PaymentService {
         },
       ],
       back_urls: {
-        success: `http://localhost:5173/user/allcredits`,
-        failure: `http://localhost:5173/user/allcredits`,
-        pending: `http://localhost:5173/user/allcredits`,
+        success: "http://localhost:5173/user/allcredits",
+        failure: "http://localhost:5173/user/allcredits",
+        pending: "http://localhost:5173/user/allcredits",
       },
       auto_return: 'approved',
       external_reference: id,
